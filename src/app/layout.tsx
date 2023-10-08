a2d80940178d6cdc1a315296b9a2144b886fe177
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-10-08 18:20:25 +0600

    ci: toaster setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 import './globals.css';
 
@@ -20,6 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <body className={cn('bg-secondary', inter.className)}>
           <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
             {children}
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
