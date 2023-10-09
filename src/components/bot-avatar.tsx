06261fcc90e414957b9f6f07770e6144930d7612
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-10-09 21:35:55 +0600

    feat: chat page

---
+++ src/components/bot-avatar.tsx
@@ -0,0 +1,11 @@
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
+
+interface BotAvatarProps {
+  src: string;
+}
+
+export const BotAvatar = ({ src }: BotAvatarProps) => (
+  <Avatar className='h-12 w-12'>
+    <AvatarImage src={src} />
+  </Avatar>
+);
